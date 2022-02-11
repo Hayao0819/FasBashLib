fb53c8ceb2fe098fe682fe4004f33a542c38ba63
hayao <hayao@fascode.net>
2022-02-12 00:32:16 +0900

    Update: Use minify

--- build.sh
+++ build.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1090
 
+# Init
 CurrentDir="$(cd "$(dirname "${0}")" || exit 1 ; pwd)"
+OutFile="${1-"${CurrentDir}/fasbashlib.sh"}"
+TmpFile="/tmp/fasbashlib.sh"
+
+# Ccnfigure dir
 SrcDir="$CurrentDir/src"
 StaticDir="$CurrentDir/static"
-OutFile="${1-"${CurrentDir}/fasbashlib.sh"}"
+LibDir="$CurrentDir/lib"
 
+# Load src
 for file in "${SrcDir}/"*".sh"; do
     echo "Load: $file"
     source "$file"
 done
 
-cat "$StaticDir/head.sh" > "$OutFile"
-typeset -f >> "$OutFile"
+# Output to temp
+cat "$StaticDir/head.sh" > "$TmpFile"
+typeset -f >> "$TmpFile"
+
+# Minify
+bash "$LibDir/minifier/Minify.sh" -f="$TmpFile" > "$OutFile"
