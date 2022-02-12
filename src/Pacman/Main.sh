d4fc5bf5a83ebf5f70f024874e0339d93ba0e18a
hayao <hayao@fascode.net>
2022-02-12 21:18:45 +0900

    Add: Add documents

--- src/Pacman/Main.sh
+++ src/Pacman/Main.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
+# @file Pacman
+# @brief Pacmanコマンドをシェルスクリプトから簡単に使えるようにしたラッパー関数
+# @description
+#    Pacman (Arch Linuxのためのパッケージマネージャ[Libalpm]のフロントエンド) をシェルスクリプトから容易に操作するためのラッパー関数
+#
+#    これらの関数はArch Linux上でのみ動作します。（動作環境の確認は現在実装されていません。）
+#
+#    一部の関数はLibalpmのファイルを直接参照します。
+#
+#    Pacmanの仕様変更などで動作しない場合は開発者に問題を報告してください。
 
+
+# @description パッケージがインストール済みかどうかを確認します。
+#
+# @example CheckPacmanPkg yay bash base
+#
+# @arg LONG  GNU getopt's long option
+# @arg SHORT GNU getopt's short option
+#
+# @exitcode 0 指定された全てのパッケージがインストールされています
+# @exitcode 1 指定されたどれかのパッケージがシステム上に見つかりませんでした
 CheckPacmanPkg(){
     local p
     for p in "$@"; do
--- src/Readlink/Main.sh
+++ src/Readlink/Main.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+# @file Readlink
+# @brief Readlink -fと同等の機能を実装した関数
+# @description
+#    readlink -fをシェルスクリプトで再実装した関数です。
+#
+#    これらのコードは [ko1nksm/readlinkf](https://github.com/ko1nksm/readlinkf) から引用しています。
+#
+#    一部インデントやコードを改変して使用させていただいています。ありがとうございます。
 
 # POSIX compliant version
 Readlinkf_Posix() {
