7a92cd28b752c9c710ea0b69ea52363cb5e97d71
hayao <hayao@fascode.net>
2022-02-12 21:35:04 +0900

    Add: Add MultiFile.sh

---
+++ bin/MuitlFile.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# Do not use FasBashLib in this file
+# shellcheck disable=SC1090,SC1091
+
+# Init
+MainDir="$(cd "$(dirname "${0}")/../" || exit 1 ; pwd)"
+OutDir="$MainDir/out"
+
+# Parse args
+while [[ -n "${1-""}" ]]; do
+    [[ "$1" == "-"* ]] || break
+    case "${1}" in
+        "-o")
+            [[ -n "${2-""}" ]] || { echo "No file is specified"; exit 1; }
+            OutDir="${2}"
+            shift 2 || break
+            ;;
+        "--")
+            shift 1
+            break
+            ;;
+        *)
+            echo "Usage: $(basename "$0") [-o Dir]"
+            [[ "${1}" = "-h" ]] && exit 0
+            exit 1
+            ;;
+    esac
+done
+
+# Ccnfigure dir
+SrcDir="$MainDir/src"
+StaticDir="$MainDir/static"
+LibDir="$MainDir/lib"
+
+# Check dir
+mkdir -p "$OutDir"
+for Dir in "$SrcDir" "$StaticDir" "$LibDir"; do
+    [[ -d "$Dir" ]]  || {
+        echo "Missing directory: $Dir"
+        exit 1
+    }
+done
+
+# Build
+# Load src
+while read -r Dir; do
+    _Lib="$(basename "$Dir")"
+    "$MainDir/bin/SingleFile.sh" -out "$OutDir/$_Lib.sh" -noreq "$_Lib"
+done < <(find "$SrcDir" -type d -mindepth 1 -maxdepth 1)
+unset Dir File
