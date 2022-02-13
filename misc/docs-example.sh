fbf7c0f34f7cab15d48db6503889c7043d8cfecc
hayao <hayao@fascode.net>
2022-02-13 23:35:09 +0900

    Add: Add document template

---
+++ misc/docs-example.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# @file File name
+# @brief File description
+# @description
+#     The project solves lots of problems:
+#      * a
+#      * b
+
+# @description My super function.
+# Not thread-safe.
+#
+# @example
+#    echo "test: $(say-hello World)"
+#
+# @arg $1 string A value to print
+# @noargs
+#
+# @set VARIABLE
+# @stdout Output string
+#
+# @exitcode 0 If successful.
+# @exitcode 1 If an empty string passed.
+#
+# @see validate()
+# @internal
