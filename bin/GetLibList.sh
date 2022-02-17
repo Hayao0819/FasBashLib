cc89bbff4b2190ab81a7379f0c814550d1c38459
hayao <hayao@fascode.net>
2022-02-18 08:18:23 +0900

    Add: Add GetLibList

---
+++ bin/GetLibList.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+# Do not use FasBashLib in this file
+# shellcheck disable=SC1090,SC1091
+
+set -Eeu
+
+# Init
+MainDir="$(cd "$(dirname "${0}")/../" || exit 1 ; pwd)"
+SrcDir="$MainDir/src"
+LibDir="$MainDir/lib"
+Quiet=false
+LibList=()
+
+# functions
+GetMeta(){
+    "$LibDir/GetMeta.sh" "$@"
+}
+
+# Parse args
+while [[ -n "${1-""}" ]]; do
+    [[ "$1" == "-"* ]] || break
+    case "${1}" in
+        "-q")
+            Quiet=true
+            shift 1
+            ;;
+        "--")
+            shift 1
+            break
+            ;;
+        *)
+            echo "Usage: $(basename "$0") [-q]"
+            [[ "${1}" = "-h" ]] && exit 0
+            exit 1
+            ;;
+    esac
+done
+LibList=("$@")
+shift "$#"
+
+if (("${#LibList[@]}" < 1 )); then
+    readarray -t LibList < <(find "${SrcDir}" -type d -mindepth 1 -maxdepth 1 -print0 | xargs -0 -L 1 basename)
+fi
+
+for Lib in "${LibList[@]}"; do
+    echo "$Lib"
+    [[ "$Quiet" = false ]] || continue
+    for C in "Description" "Depends" "Require" "Shell" "Conflicts" "Files"; do
+        echo "    $C: $(GetMeta "$Lib" "$C")"
+    done
+done
