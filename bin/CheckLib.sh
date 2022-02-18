dcc79c14536622f8d68db809fd84f9024335477f
hayao <hayao@fascode.net>
2022-02-18 13:12:13 +0900

    Fix: Do not call basename many times

--- bin/CheckLib.sh
+++ bin/CheckLib.sh
@@ -9,11 +9,13 @@ ShellList=("ModernBash" "Any" "LegacySh")
 Errors=0
 
 while read -r Dir; do
-    echo "$(basename "$Dir"): Checking ..."
+    Name="$(basename "$Dir")"
+
+    echo "${Name}: Checking ..."
 
     # ディレクトリ名とMetaのNameを確認
-    if [[ "$(basename "$Dir")" != "$("$LibDir/GetMeta.sh" "$(basename "$Dir")" "Name")" ]]; then
-        echo "$(basename "$Dir"): ディレクトリ名とMeta.iniが一致していません" >&2
+    if [[ "${Name}" != "$("$LibDir/GetMeta.sh" "${Name}" "Name")" ]]; then
+        echo "${Name}: ディレクトリ名とMeta.iniが一致していません" >&2
         Errors=$(( Errors + 1 ))
     fi
 
@@ -26,19 +28,19 @@ while read -r Dir; do
     done < <(find "$Dir" -type f)
 
     # 依存関係をテスト
-    "$LibDir/SolveRequire.sh" "$(basename "$Dir")" 1> /dev/null || Errors=$(( Errors + 1 ))
+    "$LibDir/SolveRequire.sh" "${Name}" 1> /dev/null || Errors=$(( Errors + 1 ))
 
     # シェルを確認
-    if ! printf "%s\n" "${ShellList[@]}" | grep -qx "$("$LibDir/GetMeta.sh" "$(basename "$Dir")" "Shell")"; then
-        echo "$(basename "$Dir"): 不正なシェルが設定されています" >&2
+    if ! printf "%s\n" "${ShellList[@]}" | grep -qx "$("$LibDir/GetMeta.sh" "${Name}" "Shell")"; then
+        echo "${Name}: 不正なシェルが設定されています" >&2
         Errors=$(( Errors + 1 ))
     fi
 
     # ファイルの存在を確認
-    readarray -t _FileList < <("$LibDir/GetMeta.sh" "$(basename "$Dir")" "Files")
+    readarray -t _FileList < <("$LibDir/GetMeta.sh" "${Name}" "Files")
     for File in "${_FileList[@]}"; do
         if ! [[ -e "$Dir/$File" ]]; then
-            echo "$(basename "$Dir"): $File が存在しません"
+            echo "${Name}: $File が存在しません"
             Errors=$(( Errors + 1 ))
         fi
     done
@@ -46,12 +48,12 @@ while read -r Dir; do
     # Filesに設定されていないファイル
     while read -r File; do
         if ! printf "%s\n" "${_FileList[@]}" | grep -qx "$(basename "$File")"; then
-            echo "$(basename "$Dir"): $File はライブラリとして認識されていません"
+            echo "${Name}: $File はライブラリとして認識されていません"
             Errors=$(( Errors + 1 ))
         fi
     done < <(find "$Dir" -name "*.sh" -mindepth 1)
 
-    unset File _FileList
+    unset File _FileList Name
 done < <(find "${SrcDir}" -type d -mindepth 1 -maxdepth 1 )
 
 if (( Errors == 0 )); then
