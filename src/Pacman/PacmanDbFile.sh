ca411ce81fd9bd01717fe0a406a32f3c89d01014
hayao <hayao@fascode.net>
2022-02-24 20:48:44 +0900

    Update: GetPacmanDbSection does not run RemoveBlank

--- src/Pacman/PacmanDbFile.sh
+++ src/Pacman/PacmanDbFile.sh
@@ -10,5 +10,11 @@ GetPacmanDbNextSection(){
 
 GetPacmanDbSection(){
     readarray -t _Stdin
-    PrintEvalArray _Stdin | sed -ne "/^%$1%$/,/^%$(PrintEvalArray _Stdin | GetPacmanDbNextSection "$1")%$/p" | sed "1d; \$d" | RemoveBlank
+    PrintEvalArray _Stdin | sed -ne "/^%$1%$/,/^%$(PrintEvalArray _Stdin | GetPacmanDbNextSection "$1")%$/p" | sed "1d; \$d" # | RemoveBlank
 }
+
+
+# なし
+# 6.55s user 3.90s system 179% cpu 5.816 total
+# あり
+# 7.15s user 4.22s system 195% cpu 5.821 total
