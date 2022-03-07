d0771e436fe7aebdf7bde593c98a040adc3f03e4
hayao <hayao@fascode.net>
2022-03-07 20:55:59 +0900

    Add-Test: Add test files for AddNewToArray

---
+++ tests/BetterShell/AddNewToArray/Run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+Puella_Magi_Holy_Quintet=("Madoka" "Sayaka" "Homura" "Mami" "Kyouko")
+
+# 重複して追加しない
+AddNewToArray "Puella_Magi_Holy_Quintet" "Madoka" 
+PrintEvalArray Puella_Magi_Holy_Quintet
+
+# 区切り
+echo "====="
+
+# 新しい項目を追加
+AddNewToArray "Puella_Magi_Holy_Quintet" "Nagisa"
+PrintEvalArray Puella_Magi_Holy_Quintet
