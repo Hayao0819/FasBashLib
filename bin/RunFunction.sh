086f09157658805447baf11cc921f5fa6b71e277
hayao <hayao@fascode.net>
2022-04-01 23:04:22 +0900

    Fix: new syntax funcname

--- bin/RunFunction.sh
+++ bin/RunFunction.sh
@@ -27,11 +27,13 @@ fi
 
 source /dev/stdin < <("$BinDir/SingleFile.sh" -out "/dev/stdout" "$LibName")
 
-typeset -f "${FuncName}" 1> /dev/null || {
-    echo "$FuncName is not defined." >&2
+Func="${LibName}.$FuncName"
+
+typeset -f "${Func}" 1> /dev/null || {
+    echo "$Func is not defined." >&2
     exit 1
 }
 
 echo "---Function Start---" >&2
-"$FuncName" "$@"
+"$Func" "$@"
 echo "---Function End" >&2
