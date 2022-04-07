d7e87e9770243f7151be8c6b1d2d3638467f2ef7
hayao <hayao@fascode.net>
2022-04-07 15:09:59 +0900

    Update: Add environment check

--- src/Core/Main.sh
+++ src/Core/Main.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+
+FsblibEnvCheck(){
+    case "$FSBLIB_REQUIRE" in
+        "Any")
+            return 0
+            ;;
+        "ModernShell")
+            [[ "$(cut -d "." -f 1 <<< "$BASH_VERSION")" = "5" ]] && return 0
+            ;;
+    esac
+}
