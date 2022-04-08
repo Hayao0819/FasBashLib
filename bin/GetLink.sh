106ccaf2bc03f37dc7c6bfb8636d892b0006efbd
hayao <hayao@fascode.net>
2022-04-08 22:20:36 +0900

    Update: Add tag exist check

--- bin/GetLink.sh
+++ bin/GetLink.sh
@@ -12,5 +12,10 @@ Release="${1-""}"
 if [[ -z "$Release" ]]; then
     curl -sL "https://api.github.com/repos/hayao0819/fasbashlib/releases/latest" | jq -r ".assets[].browser_download_url"
 else
+    Status="$(curl -sL -o /dev/null -w '%{http_code}\n' "https://api.github.com/repos/hayao0819/fasbashlib/releases/tags/${Release}")"
+    (( Status == 200 )) || {
+        echo "${Release}: No such tag was found"
+        exit 1
+    }
     curl -sL "https://api.github.com/repos/hayao0819/fasbashlib/releases" | jq -r ".[] | select(.tag_name == \"${Release}\") | .assets[].browser_download_url"
 fi
