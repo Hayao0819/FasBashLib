97274245ba59863120c07d30bd5815cbe81541f5
hayao <hayao@fascode.net>
2022-04-13 00:37:14 +0900

    Update: Add TextBox

--- src/BetterShell/String.sh
+++ src/BetterShell/String.sh
@@ -72,3 +72,14 @@ ToLowerStdin(){
     ForEach eval "_Str=\"{}\"; echo \"\${_Str,,}\""
     unset _Str
 }
+
+TextBox(){
+    local _Content=() _Length _Vertical="|" _Line="="
+    readarray -t _Content
+    _Length="$(PrintArray "${_Content[@]}" | awk '{ if ( length > x ) { x = length } }END{ print x }')"
+    echo "${_Vertical}${_Line}$(yes "${_Line}" | head -n "$_Length" | tr -d "\n")${_Vertical}"
+    for _Str in "${_Content[@]}"; do
+        echo "${_Vertical}${_Str}$(yes " " | head -n "$(( _Length + 1 - "${#_Str}"))" | tr -d "\n")${_Vertical}"
+    done
+    echo "${_Vertical}${_Line}$(yes "${_Line}" | head -n "$_Length" | tr -d "\n")${_Vertical}"
+}
