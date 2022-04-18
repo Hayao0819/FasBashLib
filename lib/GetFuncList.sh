57f1873d37f7e744366c8ffc10ff5486f6442101
hayao <hayao@fascode.net>
2022-04-18 16:23:44 +0900

    Update: Add -noprefix

--- lib/GetFuncList.sh
+++ lib/GetFuncList.sh
@@ -1,10 +1,36 @@
 #!/usr/bin/env bash
 MainDir="$(cd "$(dirname "${0}")/../" || exit 1 ; pwd)"
 LibDir="$MainDir/lib"
+NoPrefix=false
 if (( $# < 1 )); then
     echo "Usage: $(basename "$0") LibName"
     exit 1
 fi
+
+# Parse args
+NoArg=()
+while [[ -n "${1-""}" ]]; do
+    case "${1}" in
+        "-noprefix")
+            NoPrefix=true
+            shift 1
+            ;;
+        "--")
+            shift 1
+            break
+            ;;
+        "-"*)
+            echo "Usage: $(basename "$0") LibName"
+            [[ "${1}" = "-h" ]] && exit 0
+            exit 1
+            ;;
+        *)
+            NoArg+=("$1")
+            shift 1
+            ;;
+    esac
+done
+set -- "${NoArg[@]}"
 
 LibName="$1"
 FuncList=()
@@ -23,7 +49,7 @@ done < <("${LibDir}/GetFileList.sh" "$LibName")
 
 Prefix="$("$LibDir/GetMeta.sh" "$LibName" Prefix)"
 
-if [[ -n "${Prefix-""}" ]]; then
+if [[ -n "${Prefix-""}" ]] && [[ "$NoPrefix" = false ]]  ; then
     readarray -t FuncList < <(printf "${Prefix}.%s\n" "${FuncList[@]}")
 fi
 
