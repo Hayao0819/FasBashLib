aa4b2952af4f0286e52c996b08a8a3f9b316295c
hayao <hayao@fascode.net>
2022-04-18 16:33:16 +0900

    Update: Remove Empty dir

--- bin/RemoveUnusedTestFile.sh
+++ bin/RemoveUnusedTestFile.sh
@@ -8,15 +8,24 @@ MainDir="$(cd "$(dirname "${0}")/../" || exit 1 ; pwd)"
 TestDir="$MainDir/tests"
 
 
-RemoveFile(){
+Remove(){
     echo "Remove $1" >&2
     rm -rf "$1"
 }
 
 # Remove empty file
 while read -r File; do
+    [[ -e "$File" ]] || continue
     # shellcheck disable=SC2143
     if [[ -z "$(grep -v "^$" "$File" | grep -v "^#")" ]]; then
-        RemoveFile "$File"
+        Remove "$File"
     fi
 done < <(find "${TestDir}" -type f)
+
+# Remove empty directory
+while read -r Dir; do
+    [[ -e "$Dir" ]] || continue
+    if [[ -z "$(find "${Dir}")" ]]; then
+        Remove "$Dir"
+    fi
+done < <(find "${TestDir}" -type d)
