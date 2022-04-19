ea0638b8b3d3af8883ae32b563ed07475c4096f5
hayao <hayao@fascode.net>
2022-04-19 17:08:49 +0900

    Add: Test for BetterShell/AddNewToArray

--- tests/BetterShell/AddNewToArray/add/Run.sh
+++ tests/BetterShell/AddNewToArray/add/Run.sh
@@ -1,14 +1,5 @@
 # shellcheck disable=SC2148,SC2034
 
 Puella_Magi_Holy_Quintet=("Madoka" "Sayaka" "Homura" "Mami" "Kyouko")
-
-# 重複して追加しない
-AddNewToArray "Puella_Magi_Holy_Quintet" "Madoka" 
-PrintEvalArray Puella_Magi_Holy_Quintet
-
-# 区切り
-echo "====="
-
-# 新しい項目を追加
 AddNewToArray "Puella_Magi_Holy_Quintet" "Nagisa"
 PrintEvalArray Puella_Magi_Holy_Quintet
---
+++ tests/BetterShell/AddNewToArray/duplicate/Run.sh
@@ -0,0 +1,5 @@
+# shellcheck disable=SC2148,SC2034
+
+Puella_Magi_Holy_Quintet=("Madoka" "Sayaka" "Homura" "Mami" "Kyouko")
+AddNewToArray "Puella_Magi_Holy_Quintet" "Madoka" 
+PrintEvalArray Puella_Magi_Holy_Quintet
