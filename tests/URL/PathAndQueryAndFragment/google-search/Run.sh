588058abccaf696c4aa7dd746f6c6e3effa28618
hayao <hayao@fascode.net>
2022-06-12 10:45:29 +0900

    Add-Test: URL/PathAndQueryAndFragment

---
+++ tests/URL/PathAndQueryAndFragment/google-search/Run.sh
@@ -0,0 +1,4 @@
+# shellcheck disable=SC2148,SC2034
+MyURLExample="https://www.google.com/search?q=github.com&oq=github.com&sourceid=chrome&ie=UTF-8"
+
+URL.PathAndQueryAndFragment <<< "${MyURLExample}"
