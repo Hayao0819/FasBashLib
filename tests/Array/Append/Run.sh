67e9b7f59143499126653b66a2abd6bfcb0e1fd2
hayao <hayao@fascode.net>
2022-06-15 08:31:35 +0900

    Add-test: Array Indexof Append

---
+++ tests/Array/Append/Run.sh
@@ -0,0 +1,6 @@
+# shellcheck disable=SC2148,SC2034
+
+Puella_Magi_Holy_Quintet=("Madoka" "Sayaka" "Homura" "Mami" "Kyouko")
+
+PrintArray "Iroha" "Kanagi" "Snaa" | Array.Append "Puella_Magi_Holy_Quintet" 
+PrintEvalArray Puella_Magi_Holy_Quintet
