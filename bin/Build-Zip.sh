d70191fba29fd15a1df0f34a443035b67b0be180
hayao <hayao@fascode.net>
2022-06-17 23:27:53 +0900

    Update: Add -out

--- bin/Build-Zip.sh
+++ bin/Build-Zip.sh
@@ -13,6 +13,28 @@ TmpUpper="${TmpDir}/upper"
 TmpSnake="${TmpDir}/snake"
 TmpLower="${TmpDir}/lower"
 
+
+# Parse args
+while [[ -n "${1-""}" ]]; do
+    [[ "$1" == "-"* ]] || break
+    case "${1}" in
+        "-out")
+            [[ -n "${2-""}" ]] || { echo "No file is specified"; exit 1; }
+            OutDir="${2}"
+            shift 2 || break
+            ;;
+        "--")
+            shift 1
+            break
+            ;;
+        *)
+            echo "Usage: $(basename "$0") [-out Dir]"
+            [[ "${1}" = "-h" ]] && exit 0
+            exit 1
+            ;;
+    esac
+done
+
 mkdir -p "$TmpDir" "$OutDir"
 
 # Document
