a29061abc27e1f2f5a06a81572c33f1d2c4d559e
hayao <hayao@fascode.net>
2022-06-17 16:44:47 +0900

    Update: Support array mode

--- lib/GetMeta.sh
+++ lib/GetMeta.sh
@@ -2,10 +2,22 @@
 MainDir="$(cd "$(dirname "${0}")/../" || exit 1 ; pwd)"
 SrcDir="$MainDir/src"
 SectionName="FsbLib"
+CsvMode=false
+
+# shellcheck source=/dev/null
+source "$MainDir/lib/Common.sh"
+
 if (( $# < 2 )); then
-    echo "Usage: $(basename "$0") DirName Param [Section]"
+    echo "Usage: $(basename "$0") [-c] DirName Param [Section]"
     exit 1
-elif ((  $# >= 3)); then
+fi
+
+if [[ "$1" = "-c" ]]; then
+    CsvMode=true
+    shift 1
+fi
+
+if ((  $# >= 3)); then
     SectionName="$3"
 fi
 
@@ -24,4 +36,8 @@ MetaFile="$SrcDir/$1/Meta.ini"
     NextSection="$(printf "%s\n" "${SectionList[@]}" | grep -x "${SectionName}" -A 1 | tail -n 1)"
 
     sed -ne "/^ *\[${SectionName}\] *$/,/^ *\[${NextSection}\] *$/p" "$MetaFile" | sed "1d; s|^ *\[${NextSection}\] *$||g" | grep -v "^ *#" | grep -Ex -- "^ *${2} *=.*" | cut -d "=" -f 2- | sed "s|^ *||g; s| *$||g"  | sed "s|^\"||g; s|\"$||g"| grep -v "^$" || true
-}
+} | if [[ "$CsvMode" = true ]]; then
+    tr "," "\n" | RemoveBlank | grep -v "^$"
+else
+    cat
+fi
