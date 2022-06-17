05c45c74e974536695c287b88fd7974086203b0f
hayao <hayao@fascode.net>
2022-06-17 22:02:17 +0900

    Fix-AwkForCalc: Undefined function

--- src/AwkForCalc/Main.sh
+++ src/AwkForCalc/Main.sh
@@ -1,36 +1,41 @@
 #!/usr/bin/env bash
 
 
-AwkPrint(){
+Print(){
     awk "BEGIN {print $*}"
+}
+
+Float(){
+    local AWKSCALE="${AWKSCALE-"5"}"
+    awk "BEGIN {printf (\"%4.${AWKSCALE}f\n\", $*)}"
 }
 
 # log10(100)=2
 # Log 10 100
 Log(){
     local _Base="$1" _Number="$2"
-    @Calc "log(${_Number}) / log($_Base)"
+    @Float "log(${_Number}) / log($_Base)"
 }
 
 Pi(){
-    @Calc "atan2(0, -0)"
+    @Float "atan2(0, -0)"
 }
 
 # 180:pi = 45:x
 # 180x=45pi
 # x=
 Rad(){
-    @Calc "$1 * $(@Pi) / 180 "
+    @Float "$1 * $(@Pi) / 180 "
 }
 
 Cos(){
-    @Calc "cos($*)"
+    @Float "cos($*)"
 }
 
 Sin(){
-    @Calc "sin($*)"
+    @Float "sin($*)"
 }
 
 Tan(){
-    @Calc "sin($1)/tan($1)"
+    @Float "sin($1)/tan($1)"
 }
