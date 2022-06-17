7c8a6989350d205874b7810026a6d16c1d5d421e
hayao <hayao@fascode.net>
2022-06-17 23:22:56 +0900

    Update: Add full help document

--- tools.sh
+++ tools.sh
@@ -20,12 +20,46 @@ RunScript(){
 HelpDoc(){
     echo "FasBashLib Management Tools"
     echo
-    echo " Build Commands:"
-    echo "    docs       Build document"
-    echo "    multi      Build multi-files library"
-    echo "    run        Run funtion"
-    echo "    single     Build single-file library"
+    echo " tools.sh TYPE COMMAND [Arguments]"
     echo
+    echo " Types:"
+    echo "    build, check, list, release, test, help"
+    echo
+    echo " build Commands:"
+    echo "    docs            Build document"
+    echo "    multi           Build multi-files library"
+    echo "    run             Run funtion"
+    echo "    single          Build single-file library"
+    echo "    all             Build all files"
+    echo "    zip             Build multi file and compress"
+    echo
+    echo " check Commands:"
+    echo "    bin             Run shellcheck for management scripts"
+    echo "    lib             Run static tests with shellcheck and  FasBashLib's own"
+    echo
+    echo " list Commands:"
+    echo "    lib             Library list"
+    echo "    function | fn   Function list in library"
+    echo "    file            File list in library"
+    echo "    full | all      Full library list with description and other meta info"
+    echo "    test-todo       List of functions missing test file"
+    echo
+    echo " release Commands:"
+    echo "    link | ln       Get script URL from GitHub with API"
+    echo "    list            List of past releases"
+    echo "    note            Make release note markdown"
+    echo 
+    echo " test Commands:"
+    echo "    add             Add new files for test"
+    echo "    create | make   Create \"Result.txt\" by running \"Run.sh\""
+    echo "    try             Execute \"Run.sh\" without any updating"
+    echo "    docker          Run tests in Docker (Alpha unstable)"
+    echo "    legacy          Get library list that has old test file"
+    echo "    todo            List of functions missing test file"
+    echo "    clean           Remove empty test file"
+    echo "    rm              Remove junk file that is not used from any scripts"
+    echo "    run             Run test"
+    echo "    junk            List of junk file"
 }
 
 Main(){
@@ -134,6 +168,9 @@ Main(){
                     ;;
             esac
             ;;
+        "help")
+            HelpDoc
+            ;;
     esac || return 1
 
     if [[ "$SCRIPT_RUN" = false ]]; then
@@ -156,6 +193,10 @@ if (( $# < 2 )); then
                 ;;
             "shell" | "sh")
                 "${Args[@]:1}"
+                continue
+                ;;
+            "help")
+                HelpDoc
                 continue
                 ;;
             "" | exit)
