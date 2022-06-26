921e3af05f8593316302e02959bcaa37d7a5fd4d
hayao <hayao@fascode.net>
2022-06-26 18:15:19 +0900

    Fix-Misskey: Variable name typo

--- src/Misskey/api.sh
+++ src/Misskey/api.sh
@@ -4,6 +4,6 @@
 # Setup <Domain> <TOKEN>
 Setup(){
     export MISSKEY_DOMAIN="${1-"${MISSKEY_DOMAIN-""}"}"
-    export MISSKEY_TOKEN="${2-"${MISSKEY_DOMAIN-""}"}"
+    export MISSKEY_TOKEN="${2-"${MISSKEY_TOKEN-""}"}"
     export MISSKEY_ENTRY="https://${MISSKEY_DOMAIN}/api"
 }
