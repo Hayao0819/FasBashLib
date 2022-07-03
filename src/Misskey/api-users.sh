cc2a59448c0a594c63aa1335a17f4ee5505baedd
hayao <hayao@fascode.net>
2022-07-03 13:47:44 +0900

    Update-Misskey: Add Users.SearchByUsernameAndHost

--- src/Misskey/api-users.sh
+++ src/Misskey/api-users.sh
@@ -3,3 +3,7 @@
 Users.Notes(){
     @BindingBase "users/notes" userId -- "$@"
 }
+
+Users.SearchByUsernameAndHost(){
+    @BindingBase "users/search-by-username-and-host" username host limit detail -- "${1}" "${2-"$MISSKEY_DOMAIN"}" "${@:3}"
+}
