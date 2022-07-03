1ade4694f143fef0393682887ac839919fb86216
hayao <hayao@fascode.net>
2022-07-03 15:06:52 +0900

    Update-Misskey: Set my userid as default

--- src/Misskey/api-users.sh
+++ src/Misskey/api-users.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 Users.Notes(){
-    @BindingBase "users/notes" userId -- "$@"
+    @BindingBase "users/notes" userId -- "${1-"$(@MyId)"}" "${@:2}"
 }
 
 Users.SearchByUsernameAndHost(){
-    @BindingBase "users/search-by-username-and-host" username host limit detail -- "${1}" "${2-"$MISSKEY_DOMAIN"}" "${@:3}"
+    @BindingBase "users/search-by-username-and-host" username host limit detail -- "${1-"$(MyId)"}" "${2-"$MISSKEY_DOMAIN"}" "${@:3}"
 }
