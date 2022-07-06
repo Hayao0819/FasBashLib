20c4907426adf73b5f903cab7e41fd376920ce9e
hayao <hayao@fascode.net>
2022-07-06 17:04:47 +0900

    Update-Array: Add Array.ForEach

--- src/Array/Other.sh
+++ src/Array/Other.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-
-
-
-
+# Array.Includes "namelist" "hayao"
 Includes(){
     PrintEvalArray "$1" | grep -qx "$2"
+}
+
+
+# Array.ForEach namelist echo "{}"
+ForEach(){
+    # shellcheck disable=SC2264
+    PrintEvalArray "$1" | ForEach "${@:2}"
 }
