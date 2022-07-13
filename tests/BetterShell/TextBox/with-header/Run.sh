a98d20678dccf7c160861d156ed0582f7bda11f1
hayao <hayao@fascode.net>
2022-07-13 20:04:25 +0900

    Add-Test: BetterShell/TexxBox with header

---
+++ tests/BetterShell/TextBox/with-header/Run.sh
@@ -0,0 +1,15 @@
+# shellcheck disable=SC2148,SC2034
+
+TextBox "Message to you" << EOF
+
+Hello FasBashLib !
+
+It is the collection of functions for modern Bash
+
+I have a plan to add functions for legacy bash and posix shell
+
+I am going to continue this project while I think it is interesting
+
+This TextBox's code may not be good. Please someone improve it
+
+EOF
