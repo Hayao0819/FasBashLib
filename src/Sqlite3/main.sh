a942b7dbacdd483825de502af1dfcc2f7dadb08b
hayao <hayao@fascode.net>
2022-07-15 19:40:07 +0900

    Fix-Sqlite3: Fix debug message

--- src/Sqlite3/main.sh
+++ src/Sqlite3/main.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 Call(){
-    Msg.Debug sqlite3 "$SQLITE3_DBPATH" "$@" >&2
+    Msg.Debug sqlite3 "${SQLITE3_OPTIONS[@]}" "$SQLITE3_DBPATH" "$@" >&2
     sqlite3 "${SQLITE3_OPTIONS[@]}" "$SQLITE3_DBPATH" "$@"
 }
 
