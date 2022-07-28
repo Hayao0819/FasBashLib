933912642d5bd30dce007f47c9467a6c887c3aa9
hayao <hayao@fascode.net>
2022-07-28 22:38:44 +0900

    Update-BetterShell: Support raw bool string

--- src/BetterShell/Bool.sh
+++ src/BetterShell/Bool.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 Bool(){
+    case "$(RemoveBlank <<< "$(ToLower "$1")")" in
+        "true")
+            return 0
+            ;;
+        "false")
+            return 1
+            ;;
+    esac
     case "$(ToLower "$(PrintEval "${1}")")" in
         "true")
             return 0
