194e0d38dcb3e695f39809a84aa2ef23588e015a
hayao <hayao@fascode.net>
2022-09-02 00:46:05 +0900

    Update: Remove output directory if make fail

--- bin/Build-Install.sh
+++ bin/Build-Install.sh
@@ -83,7 +83,9 @@ _Make_GetFilesFromSourceCode(){
             echo "Failed to checkout: $Commit" >&2
             exit 1
         }
+        trap 'rm -rf "$TmpDir/archive/$Commit"; exit 1' SIGHUP SIGINT SIGQUIT SIGTERM 
         make RELEASE_DIR="$TmpDir/archive/$Commit" release
+        trap SIGHUP SIGINT SIGQUIT SIGTERM 
         git checkout "${DefaultBranchName}" || {
             echo "Failed to checkout default branch: $DefaultBranchName"
             exit 1
