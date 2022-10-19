9c5c2a30fa32babe9cd833b85b11fb38dc8b8abc
hayao <hayao@fascode.net>
2022-10-19 18:25:47 +0900

    Update-Esc: Add functions

--- src/Esc/text.sh
+++ src/Esc/text.sh
@@ -4,3 +4,104 @@ ResetStyle(){
     printf "\033[0m"
 }
 
+BlackText(){
+    printf "\033[30m"
+}
+
+RedText(){
+    printf "\033[31m"
+}
+
+GreenText(){
+    printf "\033[32m"
+}
+
+YellowText(){
+    printf "\033[33m"
+}
+
+BlueText(){
+    printf "\033[34m"
+}
+
+MagentaText(){
+    printf "\033[35m"
+}
+
+CyanText(){
+    printf "\033[36m"
+}
+
+WhiteText(){
+    printf "\033[37m"
+}
+
+BlackBackground(){
+    printf "\033[40m"
+}
+
+RedBackground(){
+    printf "\033[41m"
+}
+
+GreenBackground(){
+    printf "\033[42m"
+}
+
+YellowBackground(){
+    printf "\033[43m"
+}
+
+BlueBackground(){
+    printf "\033[44m"
+}
+
+MagentaBackground(){
+    printf "\033[45m"
+}
+
+CyanBackground(){
+    printf "\033[46m"
+}
+
+WhiteBackground(){
+    printf "\033[47m"
+}
+
+Bold(){
+    printf "\033[1m"
+}
+
+LowIntensity(){
+    printf "\033[2m"
+}
+
+Italic(){
+    printf "\033[3m"
+}
+
+Underline(){
+    printf "\033[4m"
+}
+
+Blink(){
+    printf "\033[5m"
+}
+
+RapidBlink(){
+    printf "\033[6m"
+}
+
+Reverse(){
+    printf "\033[7m"
+}
+
+Conceal(){
+    printf "\033[8m"
+}
+
+CrossedOut(){
+    printf "\033[9m"
+}
+
+
