ce988b66d223f4eb93fd75256e52daa250afca27
hayao <hayao@fascode.net>
2022-10-19 14:39:26 +0900

    Update-Esc: Add urls

--- src/Esc/Main.sh
+++ src/Esc/Main.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+
+# 参考文献
+# http://7ujm.net/etc/esc.html
+# https://zariganitosh.hatenablog.jp/entry/20150224/escape_sequence
+# https://www.mm2d.net/main/prog/c/console-02.html
+# https://qiita.com/PruneMazui/items/8a023347772620025ad6
+# https://gist.github.com/shinyaoguri/782adf1c6765dba0717745578aa9c85f
+
 ClearScreen(){
     printf "\033[2J"
 }
@@ -26,9 +34,6 @@ MoveCursorRight(){
 }
 MoveCursorLeft(){
     printf "\033[%dD" "$1"
-}
-SaveCursor(){
-    printf "\033[s"
 }
 ResetStyle(){
     printf "\033[0m"
