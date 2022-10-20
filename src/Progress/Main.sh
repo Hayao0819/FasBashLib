073e93932973a91eaed7a8150f26bb7aa2258c68
hayao <hayao@fascode.net>
2022-10-20 20:42:33 +0900

    Update-Progress: Add break line

--- src/Progress/Main.sh
+++ src/Progress/Main.sh
@@ -7,6 +7,12 @@ Rotation(){
     echo "${$}-${AnimeID}=$BASHPID" >> "$TMPDIR/$FSBLIB_PROG_PIDFILEPATH"
     while true; do
         for Chr in "-" "\\" "|" "/"; do
+            # 改行を有効化する場合
+            #printf "%s\n" "$Chr" >&2
+            #sleep 0.1
+            #Esc.MoveCursorUp 1
+            #Esc.MoveCursorRight "$((${#2}))"
+    
             printf "%s" "$Chr" >&2
             sleep 0.1
             Esc.MoveCursorLeft 1
@@ -23,3 +29,14 @@ Kill(){
 }
 
 
+Bar(){
+    local Max="$1" Counter="$2"
+    local SharpCount=$((Counter * 100 / Max))
+    local SpaceCount=$((100 - SharpCount))
+    Esc.Return
+    if (( Counter == 0 )); then
+        echo -n "$Counter/$Max [$(yes " " | head -n "$Max" | tr -d "\n")]"
+    else
+        echo -n "$Counter/$Max [$(yes "#" | head -n "$SharpCount" | tr -d "\n")$(yes " " | head -n "$SpaceCount" | tr -d "\n")]"
+    fi
+}
