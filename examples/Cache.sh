f11380ef509be4e191434be7fede0ad801e82e06
hayao <hayao@fascode.net>
2022-10-23 16:20:41 +0900

    Add-Example: Cache.sh

--- examples/Cache.sh
+++ examples/Cache.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env bash
+# shellcheck source=/dev/null
+current_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")/../" && pwd)
+source <("$current_dir/tools.sh" build single Cache -out /dev/stdout 2> /dev/null)
 
 
+# キャッシュの存在チェック
+Cache.Exist "MyTextPage" || echo "Cache does not exist."
+
+# キャッシュの作成
+Cache.Create "MyTextPage" > /dev/null << EOF
+Hello World!
+Welcome To FasBashLib
+EOF
+
+# キャッシュの取得
+Cache.Get "MyTextPage" | wc -l | sed "s| *||g"
+
+# キャッシュの存在チェック
+Cache.Exist "MyTextPage" && echo "Cache exists !"
+
+# 利用例
+date | Cache.Create "MyTextPage" > /dev/null
+sleep 2
+echo "Cached date: "
+Cache.Get "MyTextPage"
+echo "Current date: "
+date
