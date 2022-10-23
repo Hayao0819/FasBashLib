a76b02ea628c8e8a991c1199cceb4711e4a921da
hayao <hayao@fascode.net>
2022-10-23 14:03:33 +0900

    Update-Progress: Add WideBar

--- src/Progress/Main.sh
+++ src/Progress/Main.sh
@@ -8,9 +8,16 @@ Rotation(){
 }
 
 Bar(){
-    local Max="$1" Counter="$2"
-    local SharpCount=$((Counter * 100 / Max))
-    local SpaceCount=$((100 - SharpCount))
+    local Max="$1" Counter="$2" Size="${3-"100"}"
+    local SharpCount=$((Counter * Size / Max))
+    local SpaceCount=$((Size - SharpCount))
     Esc.Return
     echo -n "$Counter/$Max [$(yes "#" | head -n "$SharpCount" 2> /dev/null | tr -d "\n")$(yes " " | head -n "$SpaceCount" 2> /dev/null | tr -d "\n")]"
+}
+
+WideBar(){
+    local Max="$1" Counter="$2"
+    local StatusString="$Counter/$Max"
+    local Size=$(( $(Esc.GetTermX) - ${#StatusString} - 3 ))
+    @Bar "$Max" "$Counter" "$Size"
 }
