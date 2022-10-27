f5e0f8a08c24c3c03cbb9d17ef68f241d81c7358
hayao <hayao@fascode.net>
2022-10-27 19:13:50 +0900

    Fix-Esc: Print escape codes to /dev/tty

--- src/Esc/text.sh
+++ src/Esc/text.sh
@@ -1,107 +1,107 @@
 #!/bin/sh
 
 ResetStyle(){
-    printf "\033[0m"
+    printf "\033[0m" > /dev/tty
 }
 
 BlackText(){
-    printf "\033[30m"
+    printf "\033[30m" > /dev/tty
 }
 
 RedText(){
-    printf "\033[31m"
+    printf "\033[31m" > /dev/tty
 }
 
 GreenText(){
-    printf "\033[32m"
+    printf "\033[32m" > /dev/tty
 }
 
 YellowText(){
-    printf "\033[33m"
+    printf "\033[33m" > /dev/tty
 }
 
 BlueText(){
-    printf "\033[34m"
+    printf "\033[34m" > /dev/tty
 }
 
 MagentaText(){
-    printf "\033[35m"
+    printf "\033[35m" > /dev/tty
 }
 
 CyanText(){
-    printf "\033[36m"
+    printf "\033[36m" > /dev/tty
 }
 
 WhiteText(){
-    printf "\033[37m"
+    printf "\033[37m" > /dev/tty
 }
 
 BlackBackground(){
-    printf "\033[40m"
+    printf "\033[40m" > /dev/tty
 }
 
 RedBackground(){
-    printf "\033[41m"
+    printf "\033[41m" > /dev/tty
 }
 
 GreenBackground(){
-    printf "\033[42m"
+    printf "\033[42m" > /dev/tty
 }
 
 YellowBackground(){
-    printf "\033[43m"
+    printf "\033[43m" > /dev/tty
 }
 
 BlueBackground(){
-    printf "\033[44m"
+    printf "\033[44m" > /dev/tty
 }
 
 MagentaBackground(){
-    printf "\033[45m"
+    printf "\033[45m" > /dev/tty
 }
 
 CyanBackground(){
-    printf "\033[46m"
+    printf "\033[46m" > /dev/tty
 }
 
 WhiteBackground(){
-    printf "\033[47m"
+    printf "\033[47m" > /dev/tty
 }
 
 Bold(){
-    printf "\033[1m"
+    printf "\033[1m" > /dev/tty
 }
 
 LowIntensity(){
-    printf "\033[2m"
+    printf "\033[2m" > /dev/tty
 }
 
 Italic(){
-    printf "\033[3m"
+    printf "\033[3m" > /dev/tty
 }
 
 Underline(){
-    printf "\033[4m"
+    printf "\033[4m" > /dev/tty
 }
 
 Blink(){
-    printf "\033[5m"
+    printf "\033[5m" > /dev/tty
 }
 
 RapidBlink(){
-    printf "\033[6m"
+    printf "\033[6m" > /dev/tty
 }
 
 Reverse(){
-    printf "\033[7m"
+    printf "\033[7m" > /dev/tty
 }
 
 Conceal(){
-    printf "\033[8m"
+    printf "\033[8m" > /dev/tty
 }
 
 CrossedOut(){
-    printf "\033[9m"
+    printf "\033[9m" > /dev/tty
 }
 
 
