bb08529d3528ff0d82dcaddfbb785d622fcbe76d
hayao <hayao@fascode.net>
2023-01-08 23:28:20 +0900

    Update: Document generater

--- bin/Build-Docs.sh
+++ bin/Build-Docs.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2154
 
+TmpFile="$(mktemp)"
+
 # shellcheck source=/dev/null
 source "$(cd "$(dirname "${0}")/../" || exit 1 ; pwd)/lib/Common.sh"
+
+readarray -t LibList < <(GetLibList)
 
 # Parse args
 while [[ -n "${1-""}" ]]; do
@@ -59,10 +63,34 @@ ReplaceTextInArray(){
     readarray -t "$ArrayName" < <(eval "PrintArray \"\${${ArrayName}[@]}\"" | sed "s|$Text|$Replace|g")
 }
 
+isFuncDefLine(){
+    grep -Eq ".*\(\) *{? *$" <<< "$1" && ! grep -Eq -- "=\(\)" <<< "$1"
+}
+
+getFuncName(){
+    sed -E "s| *\(\) *\{? *$||g; s|^ *||g; s| *$||g; s|^	*||g; s|	*$||g; /^$/d" <<< "$1"
+}
+
+isDocLine(){
+    grep -Eq "^ *# *@.*$" <<< "$1"
+}
+
+formatDocLine(){
+    sed "s|^ *# *||g; s| *$||g; s|^	*||g; s|	*$||g; /^$/d; s|^@||g; s|@.*$||g" <<< "$1"
+}
+
 GenerateDoc(){
     local LibName="$1"
-    local Section="" MultiLine=false Value=() Line="" ParsedLine=() Function=""
-    local DocumentBody=()
+    local Section="" MultiLine=false Key="" Value="" Line=""
+    local DocumentBody=() FunctionsBody=() IndexBody=()
+    local DocCount=0 DocumentedFuncList=()
+
+    local FUNC_ARG=() FUNC_STDOUT=() FUNC_EXITCODE=() FUNC_DESC=() FUNC_EXAMPLE=()
+
+    if ! [[ -e "$SrcDir/$LibName/Description.txt" ]]; then
+        echo "No description file: $SrcDir/$LibName/Description.txt"
+        return 1
+    fi
 
     # Get template
     readarray -t DocumentBody < <(cat "$StaticDir/document-body.md")
@@ -72,36 +100,138 @@ GenerateDoc(){
     ReplaceTextInArray DocumentBody "%LIBRARY_DESCRIPTIONTXT%" "$(cat "$SrcDir/$LibName/Description.txt")"
     ReplaceTextInArray DocumentBody "%LIBRARY_META_DESCRIPTION%" "$("$LibDir/GetMeta.sh" "$LibName" Description)"
 
-    PrintArray "${DocumentBody[@]}"
-    exit 0
-
     # Parse script code
     while read -r Line; do
         # get function
-        if grep -Eq ".*() *{ *$" <<< "$Line"; then
-            Section="$(sed "s| *() *{ *$||g; s|^ *||g; s| *$||g; s|^	*||g; s|	*$||g; /^$/d" <<< "$Line")"
-            echo "## $Section"
-        elif grep -Eq "^ *# *@.*$" <<< "$Line"; then
-            Line="$(sed "s|^ *# *||g; s| *$||g; s|^	*||g; s|	*$||g; /^$/d; s|^@||g; s|@.*$||g" <<< "$Line")"
-            readarray -t ParsedLine < <(tr " " "\n" <<< "$Line")
-            Value=("${ParsedLine[@]:1}")
-            
+        if isFuncDefLine "$Line"; then
+            # Get function name
+            Section="$(getFuncName "$Line")"
+
+            if (( ${#FUNC_DESC[@]} > 0)); then
+                DocumentedFuncList+=("$Section")
+            fi
+
+            # Write document
+            readarray -O "${#FunctionsBody[@]}" FunctionsBody < <(
+                if (( ${#FUNC_DESC[@]} > 0)); then
+                    DocumentedFuncList+=("$Section")
+                    echo "### $Section"
+                fi
+                if (( ${#FUNC_DESC[@]} > 0)); then
+                    echo "${FUNC_DESC[@]}"
+                fi
+                if (( ${#FUNC_ARG[@]} > 0)); then
+                    echo "#### Arguments"
+                    echo
+                    echo "${FUNC_ARG[@]}"
+                fi
+                if (( ${#FUNC_STDOUT[@]} > 0)); then
+                    echo "#### Stdout"
+                    echo
+                    echo "${FUNC_STDOUT[@]}"
+                fi
+                if (( ${#FUNC_EXITCODE[@]} > 0)); then
+                    echo "#### Exitcode"
+                    echo
+                    echo "${FUNC_EXITCODE[@]}"
+                fi
+                if (( ${#FUNC_EXAMPLE[@]} > 0)); then
+                    echo "#### Example"
+                    echo
+                    echo '```bash'
+                    echo "${FUNC_EXAMPLE[@]}"
+                    echo '```'
+                fi
+            )
+
+            FUNC_DESC=() FUNC_ARG=() FUNC_STDOUT=() FUNC_EXITCODE=() FUNC_EXAMPLE=()
+            DocCount=0
+
+        elif isDocLine "$Line"; then
+            Line="$(formatDocLine "$Line")"
+            Key="$(cut -d " " -f 1 <<< "$Line")"
+            Value="$(cut -d " " -f 2- <<< "$Line")"
+            DocCount=$((DocCount+1))
+
+            case "$Key" in
+                "description")
+                    [[ -n "$Value" ]] && FUNC_DESC+=("$Value")
+                    MultiLine=true
+                    ;;
+                "example")
+                    #FUNC_EXAMPLE+=("$Value")
+                    MultiLine=true
+                    ;;
+                "arg")
+                    FUNC_ARG+=("$Value")
+                    MultiLine=false
+                    ;;
+                "noarg" | "noargs")
+                    FUNC_ARG=("No arguments")
+                    MultiLine=false
+                    ;;
+                "stdout")
+                    FUNC_STDOUT+=("$Value")
+                    MultiLine=false
+                    ;;
+                "exitcode")
+                    FUNC_EXITCODE+=("$Value")
+                    MultiLine=false
+                    ;;
+                *)
+                    echo "Unknown key: $Key"
+                    ;;
+            esac
+        
+        elif [[ "$MultiLine" == true ]]; then
+            Line="$(formatDocLine "$Line")"
+            case "$Key" in
+                "description")
+                    FUNC_DESC+=("$Line")
+                    ;;
+                "example")
+                    FUNC_EXAMPLE+=("$Line")
+                    ;;
+                *)
+                    echo "Unknown key: $Key"
+                    ;;
+            esac
 
         fi 
     done < <(GetFullShellCode "$LibName")
+
+    for Func in "${DocumentedFuncList[@]}"; do
+        IndexBody+=("* [$Func](#${Func,,})")
+    done
+
+    # Write index
+    printf "%s\n" "${IndexBody[@]}" > "$TmpFile"
+    readarray DocumentBody < <(printf '%s\n' "${DocumentBody[@]}" | sed -e "/^%DOCUMENT_INDEX%$/r $TmpFile")
+
+    # Write functions
+    printf "%s\n" "${FunctionsBody[@]}" > "$TmpFile"
+    readarray DocumentBody < <(printf '%s\n' "${DocumentBody[@]}" | sed -e "/^%DOCUMENT_FUNCTIONS%$/r $TmpFile" )
+
+
+    #printf '%s\n' "${DocumentBody[@]}" > "${DocsDir}/${LibName}.md"
+    if [[ -n "${DocumentBody[*]}" ]]; then
+        printf '%s\n' "${FunctionsBody[@]}" > "${DocsDir}/${LibName}.md"
+    fi
 }
 
 PrepareBuild
 
 
-LibList=("BetterShell")
+#LibList=("BetterShell")
 
 for Lib in "${LibList[@]}"; do
     #echo > "${DocsDir}/${Lib}.md"
 
-    GenerateDoc "$Lib"
+    echo "Generating doc for $Lib" >&2
+    GenerateDoc "$Lib" || echo "Failed to generate doc for $Lib" >&2
 
     #| gawk -f "$LibDir/shdoc/shdoc" >> "${DocsDir}/${Lib}.md"
 done
+wait
 
 
